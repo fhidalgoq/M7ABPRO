c5aba2594f915aeee04ab3e1f9c92c0a149c32d5
fhidalgoq <francisco.hidalgo16@gmail.com>
2025-10-28 11:29:40 -0300

    cambio

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import axios from 'axios'
 
 const app = createApp(App)
 const pinia = createPinia()
